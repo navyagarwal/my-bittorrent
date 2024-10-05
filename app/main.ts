cb3b68ef0035698c579219285c94a2fef04b83fc
Navya Agarwal <82928853+navyagarwal@users.noreply.github.com>
2024-10-05 20:49:24 +0000

    Simplify code and resolve errors

--- app/main.ts
+++ app/main.ts
@@ -1,63 +1,55 @@
-function decodeBencode(bencodedValue: string): string | number | Array<any> {
+function decodeBencode(bencodedValue: string): [string | number | Array<any> | Map<any, any>, number] {
     if (!isNaN(parseInt(bencodedValue[0]))) {
         return decodeBencodeString(bencodedValue);
     } else if (bencodedValue[0] == 'i' && bencodedValue[bencodedValue.length-1] == 'e') {
         return decodeBencodeNumber(bencodedValue);
     } else if (bencodedValue[0] == 'l' && bencodedValue[bencodedValue.length-1] == 'e') {
         return decodeBencodeList(bencodedValue);
+    } else if (bencodedValue[0] == 'd' && bencodedValue[bencodedValue.length-1] == 'e') {
+        return decodeBencodeDictionary(bencodedValue);
     } else {
         throw new Error("Only strings and numbers are supported at this moment.");
     }
 }
 
-function decodeBencodeString(bencodedValue: string): string {
+function decodeBencodeString(bencodedValue: string): [string, number] {
     const firstColonIndex = bencodedValue.indexOf(":");
     if (firstColonIndex === -1) {
         throw new Error("Invalid encoded value");
     }
     const lenStr = bencodedValue.substring(0, firstColonIndex);
     const len = parseInt(lenStr);
-    return bencodedValue.substring(firstColonIndex + 1, len);
+    return [bencodedValue.substring(firstColonIndex + 1, firstColonIndex + 1 + len), len];
 }
 
-function decodeBencodeNumber(bencodedValue: string): number {
-    const numberStr = bencodedValue.substring(1, bencodedValue.length - 2);
+function decodeBencodeNumber(bencodedValue: string): [number, number] {
+    const firstEIndex = bencodedValue.indexOf("e");
+    const numberStr = bencodedValue.substring(1, firstEIndex);
     const ans = parseInt(numberStr);
-    return ans;
+    return [ans, firstEIndex-1];
 }
 
-function decodeBencodeList(bencodedValue: string): Array<any> {
+function decodeBencodeList(bencodedValue: string): [Array<any>, number] {
     const ans = new Array<any>;
-    let listStr = bencodedValue.substring(1, bencodedValue.length - 2);
-    while(listStr.length > 0){
-        if(!isNaN(parseInt(listStr[0]))){
-            const lenStr = bencodedValue.substring(0, listStr.indexOf(":"));
-            ans.push(decodeBencodeString(listStr));
-            listStr = listStr.substring(parseInt(lenStr)+lenStr.length+1);
-        } else {
-            const first_e_index = listStr.indexOf("e");
-            if(listStr[0] == 'i'){
-                ans.push(decodeBencodeNumber(listStr.substring(0, first_e_index+1)));
-            } else {
-                ans.push(decodeBencodeList(listStr.substring(0, first_e_index+1)));
-            }
-            listStr = listStr.substring(first_e_index+1);
-        }
+    let index = 1;
+    while(bencodedValue[index] != 'e'){
+        bencodedValue = bencodedValue.substring(index);
+        const [parsedStr, len] = decodeBencode(bencodedValue);
+        index += len;
     }
-    return ans;
+    return [ans, index+1];
 }
 
-function decodeBencodeDictionary(bencodedValue: string): Map<any, any> {
+function decodeBencodeDictionary(bencodedValue: string): [Map<any, any>, number] {
     let dictionary = new Map<any, any>;
-    const list = decodeBencodeList(bencodedValue);
-    if(list.length%2 == 0){
-        let i = 0;
-        while(i < list.length){
-            dictionary.set(list[i], list[i+1]);
-            i += 2;
-        }
+    let index = 1;
+    while(bencodedValue[index] != 'e'){
+        const [key, keyLength] = decodeBencode(bencodedValue.substring(index));
+        index += keyLength;
+        const [value, valueLength] = decodeBencode(bencodedValue.substring(index));
+        index += valueLength;
     }
-    return dictionary;
+    return [dictionary, index+1];
 }
 
 const args = process.argv;
