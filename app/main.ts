c02c270eb01dba62c40a423eacc103c4f8206678
Navya Agarwal <82928853+navyagarwal@users.noreply.github.com>
2024-10-05 16:00:01 +0000

    Decode bencoded integers

--- app/main.ts
+++ app/main.ts
@@ -1,22 +1,33 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string {
+function decodeBencode(bencodedValue: string): string | number {
+    if (!isNaN(parseInt(bencodedValue[0]))) {
+        return decodeBencodeString(bencodedValue);
+    } else if (bencodedValue[0] == 'i' && bencodedValue[-1] == 'e') {
+        return decodeBencodeNumber(bencodedValue);
+    } else {
+        throw new Error("Only strings and numbers are supported at this moment.");
+    }
+}
+
+function decodeBencodeString(bencodedValue: string): string {
     /* This function is used to decode a bencoded string
     The bencoded string is a string that is prefixed by the length of the string
     **/
-
-    // Check if the first character is a digit
-    if (!isNaN(parseInt(bencodedValue[0]))) {
-        const firstColonIndex = bencodedValue.indexOf(":");
-        if (firstColonIndex === -1) {
-            throw new Error("Invalid encoded value");
-        }
-        return bencodedValue.substring(firstColonIndex + 1);
-    } else {
-        throw new Error("Only strings are supported at the moment");
+    const firstColonIndex = bencodedValue.indexOf(":");
+    if (firstColonIndex === -1) {
+        throw new Error("Invalid encoded value");
     }
+    return bencodedValue.substring(firstColonIndex + 1);
 }
+
+function decodeBencodeNumber(bencodedValue: string): number {
+    const numberStr = bencodedValue.substring(1, bencodedValue.length - 2);
+    const ans = parseInt(numberStr);
+    return ans;
+}
+
 
 const args = process.argv;
 const bencodedValue = args[3];
