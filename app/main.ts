d3edde976967880897be3b6ca6e8d3f00efd24d1
Navya Agarwal <82928853+navyagarwal@users.noreply.github.com>
2024-10-05 17:07:49 +0000

    Decode bencoded lists

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,13 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string | number {
+function decodeBencode(bencodedValue: string): string | number | Array<any> {
     if (!isNaN(parseInt(bencodedValue[0]))) {
         return decodeBencodeString(bencodedValue);
-    } else if (bencodedValue[0] == 'i' && bencodedValue[-1] == 'e') {
+    } else if (bencodedValue[0] == 'i' && bencodedValue[bencodedValue.length-1] == 'e') {
         return decodeBencodeNumber(bencodedValue);
+    } else if (bencodedValue[0] == 'l' && bencodedValue[bencodedValue.length-1] == 'e') {
+        return decodeBencodeList(bencodedValue);
     } else {
         throw new Error("Only strings and numbers are supported at this moment.");
     }
@@ -28,6 +30,25 @@ function decodeBencodeNumber(bencodedValue: string): number {
     return ans;
 }
 
+function decodeBencodeList(bencodedValue: string): Array<any> {
+    const ans = new Array<any>;
+    let listStr = bencodedValue.substring(1, bencodedValue.length - 2);
+    while(listStr.length > 0){
+        if(!isNaN(parseInt(listStr[0]))){
+            ans.push(decodeBencodeString(listStr.substring(0, parseInt(listStr[0])+2)));
+            listStr = listStr.substring(parseInt(listStr[0])+3);
+        } else {
+            const first_e_index = bencodedValue.indexOf("e");
+            if(listStr[0] == 'i'){
+                ans.push(decodeBencodeNumber(listStr.substring(0, first_e_index+1)));
+            } else {
+                ans.push(decodeBencodeList(listStr.substring(0, first_e_index+1)));
+            }
+            listStr = listStr.substring(first_e_index+1);
+        }
+    }
+    return ans;
+}
 
 const args = process.argv;
 const bencodedValue = args[3];
